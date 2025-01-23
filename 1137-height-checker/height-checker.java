faae2e32209f1fceec2c36cfd823ba7d6e81a994
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-01-23 14:10:00 +0530

    Time: 4 ms (3.76%) | Memory: 41.8 MB (25.04%) - LeetSync

--- 1137-height-checker/height-checker.java
+++ 1137-height-checker/height-checker.java
@@ -1,6 +1,7 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int[] h1=new int [heights.length];
+        // int[] h1=new int [heights.length];
+        int[] h1= heights.clone();
         int c=0;
         for (int i=0;i<heights.length;i++){
             h1[i]=heights[i];
