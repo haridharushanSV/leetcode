efd3a5091840926723ea5180ba8906351ae0162a
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-01-24 12:05:19 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (75.22%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.java
@@ -1,26 +1,10 @@
 class Solution {
     public boolean isSameAfterReversals(int num) {
-    //   int revNum=0,revNum1=0,rev=0;
-    //   int n=num;
-    //     while(n>0){
-    //          revNum = revNum * 10 + n % 10;
-    //         n = n / 10;
-            
-    //     }
-    //     rev=revNum;
-    //      while(rev>0){
-    //          revNum1 = revNum1 * 10 + rev % 10;
-    //         rev = rev / 10;
-            
-    //     }
-
-    //     System.out.println(revNum1);
-    //             System.out.println(num);
-
-    
-    // return num==revNum1;
     int a=num%10;
-    // if(num<10){
+    return (num < 10) ? true : (num%10 == 0) ? false : true;
+}}
+
+   // if(num<10){
     //     return true;
     // }
     // else if(a==0){
@@ -28,5 +12,3 @@ class Solution {
     // }
    // return true;
    // return num<10?a==0:true;
-    return (num < 10) ? true : (a == 0) ? false : true;
-}}
