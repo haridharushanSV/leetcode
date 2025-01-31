8efdcec4e6c71e48e7a14b131593a2ce66b0b7d7
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-01-31 16:13:42 +0530

    Time: 19 ms (13.40%) | Memory: 45.1 MB (25.10%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
                String newStr =s.replaceAll("[\\s\\p{Punct}\\W_]", "").toLowerCase();
-           //    String newStr1 =s.replaceAll("[\\s,:;]", "").toLowerCase();
         StringBuilder str=new StringBuilder(newStr);
         boolean ist=false;
         str.reverse();
         String a=str.toString();
-        System.out.println(str);
-                System.out.println(newStr);
-                                System.out.println(str.equals(newStr));
-
-
         if(newStr.equals(a)||newStr.equals(" ")){
             ist= true;
         }
