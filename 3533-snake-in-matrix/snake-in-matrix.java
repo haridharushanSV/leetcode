ede878fb59191d8227037c4fe987fd7fce315d22
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-03 19:04:40 +0530

    Time: 2 ms (93.95%) | Memory: 44.1 MB (92.07%) - LeetSync

---
+++ 3533-snake-in-matrix/snake-in-matrix.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int row = 0, col = 0;
+        for (int i = 0; i < commands.size(); i++) {
+            switch (commands.get(i)) {
+                case "LEFT":
+                    col--;
+                    break;
+                case "RIGHT":
+                    col++;
+                    break;
+                case "DOWN":
+                    row++;
+                    break;
+                case "UP":
+                    row--;
+            }
+        }
+        return (row * n) + col;
+    }
+}
