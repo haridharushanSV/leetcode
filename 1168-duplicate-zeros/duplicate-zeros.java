57154bdd15450cefc50d2665414ae751f1cf5dc6
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-03 11:20:49 +0530

    Time: 13 ms (38.36%) | Memory: 44.6 MB (77.62%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,17 +1,13 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
-       int n = arr.length;
-        
-        // Traverse the array
-        for (int i = 0; i < n; i++) {
-            // If a zero is encountered
-            if (arr[i] == 0) {
-                // Shift elements to the right, starting from the end
-                for (int j = n - 1; j > i; j--) {
-                    arr[j] = arr[j - 1];
-                }
-                // Duplicate the zero by setting the next element to zero
-                i++;  // Skip over the next position as it contains the duplicated zero
+        int l=arr.length;
+        for(int i=0;i<l-1;i++){
+            if(arr[i]==0){
+              for(int j=l-2;j>i;j--){
+                arr[j+1]=arr[j];
+              }
+              arr[i+1]=0;
+                i++;
             }
         }
     }
