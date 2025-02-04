6aa28676b76538dac4449b29950a8a7c907042e3
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-04 20:56:37 +0530

    Time: 2 ms (21.14%) | Memory: 42 MB (8.10%) - LeetSync

---
+++ 500-keyboard-row/keyboard-row.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        ArrayList<String> ans = new ArrayList<String>();
+        for(int i=0;i<words.length;++i){    
+            String s = words[i].toLowerCase();
+            if(s.matches("^[qwertyuiop]*$") || s.matches("^[asdfghjkl]*$") || s.matches("^[zxcvbnm]*$")){
+                ans.add(words[i]);
+            }
+        }
+        String[] ret = new String[ans.size()];
+        ret = ans.toArray(ret); 
+        return ans.toArray(ret);
+    }
+}
