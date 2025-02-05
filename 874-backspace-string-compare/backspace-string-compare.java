8495b730676e9f5ff0d85434d6f3567e81fe4e99
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-05 22:20:38 +0530

    Time: 2 ms (54.34%) | Memory: 41.6 MB (67.56%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,26 +1,10 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-    int n = s.length();
- 
-//     // To point at position after considering the
-//     // backspaces
-//     int idx = 0;
-//  StringBuilder str=new StringBuilder();
-//     for (int i = 0; i < n-1; i++) {
-//       if (s.charAt(i+1) == '#') {
-// continue;     //   idx++;
-//       }
-//       else{
-//                  str.append(s.charAt(i));
-
-//       }
-//       }
-//      System.out.println(str);
-//     return true;
+  
 Stack<Character> st=new Stack<>();
 Stack<Character> st2=new Stack<>();
 
-for(int i = 0; i < n; i++){
+for(int i = 0; i < s.length(); i++){
     if(s.charAt(i)!='#'){
         st.push(s.charAt(i));
     }
