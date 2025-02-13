dc09519392566a4b2afb8a4024286a21b8226465
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-13 09:45:13 +0530

    Time: 5 ms (10.82%) | Memory: 42.2 MB (18.50%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static boolean isInteger(String str) {
+    try {
+        Integer.parseInt(str);
+        return true;
+    } catch (NumberFormatException e) {
+        return false;
+    }
+}
+
+    public int calPoints(String[] operations) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<operations.length;i++){
+            if(operations[i].equals("+")){
+                int b=st.pop();
+                int c=b+st.peek();
+                st.push(b);
+                st.push(c);
+            }
+           
+             
+              else  if(operations[i].equals("D")){
+                    int a=st.peek();
+        
+                    st.push(a*2);
+                }
+                 else  if(operations[i].equals("C")){
+                    st.pop();
+                }
+                else{
+                      if (isInteger(operations[i])) {
+    st.push(Integer.parseInt(operations[i]));}
+                }
+            
+        }
+        System.out.println(st);
+         int sum = 0;
+        for (int score : st) {
+            sum += score;
+        }
+        return sum;
+    }
+}
