ec0ab2614aad42617b5f37e55a55df14a899660f
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-17 10:45:44 +0530

    Time: 2 ms (87.75%) | Memory: 41.5 MB (85.91%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.java
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.java
@@ -1,23 +1,26 @@
 class Solution {
-    public int numTilePossibilities(String tiles) {
-          int[] freq = new int[26];// Frequency array for letters A-Z
-        for (char c : tiles.toCharArray()) //loop iterates over each character c in the input string tiles.
-        {
-            freq[c - 'A']++;
-//c - 'A' converts the character c to an index.freq[c - 'A']++ increments the count of that letter.
-        }
-        return backtrack(freq);
+  public int numTilePossibilities(String tiles) {
+    int[] count = new int[26];
+
+    for (final char t : tiles.toCharArray())
+      ++count[t - 'A'];
+
+    return dfs(count);
+  }
+
+  private int dfs(int[] count) {
+    int possibleSequences = 0;
+
+    for (int i = 0; i < 26; ++i) {
+      if (count[i] == 0)
+        continue;
+      // Put c in the current position. We only care about the number of possible
+      // sequences of letters but don't care about the actual combination.
+      --count[i];
+      possibleSequences += 1 + dfs(count);
+      ++count[i];
     }
-    private int backtrack(int[] freq) {
-        int count = 0;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                freq[i]--;
-                count += 1 + backtrack(freq);
-                freq[i]++;
-            }
-        }
-        return count;
-//dry run this method if not understand :)
-    }
+
+    return possibleSequences;
+  }
 }
