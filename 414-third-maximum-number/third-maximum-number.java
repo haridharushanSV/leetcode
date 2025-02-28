1f16cc7a610f59fe91e6793ef2f1fd0668a57207
Haridharushan S V <143950251+haridharushanSV@users.noreply.github.com>
2025-02-28 09:43:43 +0530

    Time: 5 ms (40.37%) | Memory: 44.5 MB (36.32%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+       public int thirdMax(int[] nums) {
+        //init  treeSet
+        //populate set
+        //if the set size >= 3 pop off first 2 maxes
+        //then return last element
+        
+        TreeSet<Integer> set = new TreeSet<>();
+    
+        for(int i : nums)
+        {
+            set.add(i);
+        }
+        
+        if(set.size() >= 3)
+        {
+            set.pollLast();
+            set.pollLast();
+        }
+        
+        return set.last();
+    }
+}
